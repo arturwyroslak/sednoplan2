b26751a023788848f33795fb1e8e8fea95183650
arturwyroslak <95839215+arturwyroslak@users.noreply.github.com>
2023-07-07 22:59:09 +0200

    Create admin.js

---
+++ public/admin.js
@@ -0,0 +1,34 @@
+// Inicjalizacja edytorów Quill
+var quillEditors = {};
+
+var editorElements = document.querySelectorAll('.quill-editor');
+for (var i = 0; i < editorElements.length; i++) {
+    var id = editorElements[i].id;
+    quillEditors[id] = new Quill('#' + id, {
+        theme: 'snow'
+    });
+}
+
+// Obsługa zapisywania zmian
+document.querySelector('#save-button').addEventListener('click', function() {
+    for (var id in quillEditors) {
+        var content = quillEditors[id].getContents();
+
+        // Wyślij żądanie do backendu o zapisanie treści
+        fetch('https://sednoplan.vercel.app/modals/' + id, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ content: content })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                console.log('Zapisano treść modalu ' + id);
+            } else {
+                console.error('Nie udało się zapisać treści modalu ' + id);
+            }
+        });
+    }
+});
