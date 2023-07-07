b4def66aa86ffa5afa4df1e62d4c56cd295a7521
arturwyroslak <95839215+arturwyroslak@users.noreply.github.com>
2023-07-07 22:54:53 +0200

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,21 @@
+// Pobierz wszystkie ikony informacyjne
+var infoIcons = document.querySelectorAll('.fas.fa-info-circle');
+
+// Dodaj zdarzenie kliknięcia do każdej ikony
+for (var i = 0; i < infoIcons.length; i++) {
+    infoIcons[i].addEventListener('click', function(event) {
+        // Pobierz ID modalu z atrybutu data-target
+        var modalId = event.target.dataset.target;
+
+        // Wyślij żądanie do backendu o pobranie treści dla modalu
+        fetch('https://your-backend.com/modals/' + modalId)
+            .then(response => response.json())
+            .then(data => {
+                // Aktualizuj treść modalu
+                document.querySelector(modalId + ' .modal-body').textContent = data.content;
+
+                // Wyświetl modal
+                $(modalId).modal('show');
+            });
+    });
+}
