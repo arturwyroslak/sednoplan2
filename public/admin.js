3784f94c75da28c0e95c0908a3f879b499dd3292
arturwyroslak <95839215+arturwyroslak@users.noreply.github.com>
2023-07-08 00:25:38 +0200

    Update admin.js

--- public/admin.js
+++ public/admin.js
@@ -12,26 +12,27 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Obsługa zapisywania zmian dla każdego edytora
     for (var id in quillEditors) {
-        document.querySelector('#save-button' + id.replace('modal', '')).addEventListener('click', function() {
-            var content = quillEditors[id].getContents();
+        (function(id) {
+            document.querySelector('#save-button' + id.replace('modal', '')).addEventListener('click', function() {
+                var content = quillEditors[id].getContents();
 
-            // Wyślij żądanie do backendu o zapisanie treści
-            fetch('http://localhost:3000/modals/' + id, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ content: content })
-})
-
-            .then(response => response.json())
-            .then(data => {
-                if (data.success) {
-                    console.log('Zapisano treść modalu ' + id);
-                } else {
-                    console.error('Nie udało się zapisać treści modalu ' + id);
-                }
+                // Wyślij żądanie do backendu o zapisanie treści
+                fetch('http://localhost:3000/modals/' + id, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ content: content })
+                })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success) {
+                        console.log('Zapisano treść modalu ' + id);
+                    } else {
+                        console.error('Nie udało się zapisać treści modalu ' + id);
+                    }
+                });
             });
-        });
+        })(id);
     }
 });
